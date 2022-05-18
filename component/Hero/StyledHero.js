06ccef12f3143b3ea245baa5adcc88f2346918f7
dragg54 <ajibolasadiq@yahoo.com>
2022-05-18 23:51:15 +0100

    completed the hero section

--- component/Hero/StyledHero.js
+++ component/Hero/StyledHero.js
@@ -9,14 +9,21 @@ export const HeroContainer = styled.div`
     background-repeat: no-repeat;
     `
 export const HeaderContainer = styled.div`
-    width:1000px;
+    width:100%;
     height:100px;
     display:flex;
     justify-content: space-between;
     align-items: center;
+    padding: 16px;
 `
+
+export const NavAccountContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+`   
+
 export const Logo = styled.h2`
-    color: #FF4500;
+    color: #FF6F1D;
     font-size: 30px;
     font-weight: bolder;
 `
@@ -30,7 +37,37 @@ export const NavList = styled.ul`
 `
 
 export const NavElements = styled.li`
-    font-size: 0.8rem;
+    font-size: 1rem;
     color: white;
     margin-right: 16px
+`
+
+export const HeroCaptionContainer = styled.div`
+    width: 40%;
+    height: 600px;
+    padding:50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`
+
+export const HeroPrimaryCaption = styled.h1`
+    font-size: 3.4rem;
+    font-weight: bold;
+    color: white;
+    margin-bottom: 10px;
+`
+
+export const HeroSecondaryCaption = styled.p`
+    font-size: 1.2rem;
+    color: white;
+`
+
+export const PrimaryButton = styled.button`
+    font-size: 0.7rem;
+    padding: 18px 12px;
+    background-color: #FF6F1D;
+    width: 300px;
+    color: white;
+    border: none
 `
