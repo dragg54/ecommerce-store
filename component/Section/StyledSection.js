c86d3f36ae08a87ddca73914ead39fa1e2570bb2
dragg54 <ajibolasadiq@yahoo.com>
2022-05-21 17:47:08 +0100

    Linked pages

--- component/Section/StyledSection.js
+++ component/Section/StyledSection.js
@@ -1,32 +1,41 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const ImageContainer = styled.div`
-    width: 500px;
-    height: 700px;
+  width: ${(props) => (props.primary ? "200px" : "300px")};
+  height: ${(props) => (props.primary ? "450px" : "700px")};
+  cursor: pointer;
+
+  @media (max-width: 990px) {
     margin: 8px;
-`
+  }
+`;
 export const ImageSection = styled.div`
-  display: flex;
+  display: ${props=>props.primary? "grid":"flex"};
+  grid-template-columns: repeat(3, 1fr);
+  row-gap: 70px;
+  column-gap: 150px;
   justify-content: space-around;
   align-items: center;
   padding: 50px;
 
-  @media(max-width: 990px){
-      padding: 40px 20px;
-      justify-content: space-between;
+
+  @media (max-width: 990px) {
+    padding: 40px 20px;
+    justify-content: space-between;
   }
 `;
 
 export const ImageCaption = styled.p`
-  font-weight: bolder;
+  font-weight: ${props=>props.primary? "": "bolder"};
   cursor: pointer;
-  @media(max-width:990px){
-      font-size:12px;
+  font-size: 14px;
+  @media (max-width: 990px) {
+    font-size: 12px;
   }
-`
+`;
 
 export const ImageNumber = styled.p`
   color: orange;
   font-size: 12px;
   font-weight: bolder;
-`
+`;
