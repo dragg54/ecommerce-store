cfb687e2120f712715b9e8e70ec3f493218c8f64
dragg54 <ajibolasadiq@yahoo.com>
2022-05-21 17:48:28 +0100

    initial commit

--- pages/Men/index.js
+++ pages/Men/index.js
@@ -1,31 +1,90 @@
 import Image from "next/image";
+import {
+  ImageCaption,
+  ImageContainer,
+  ImageNumber,
+  ImageSection,
+} from "../../component/Section/StyledSection";
+import {
+  HeaderContainer,
+  Logo,
+  NavList,
+  NavElements,
+} from "../../component/Hero/StyledHero";
+import { FiSearch, FiShoppingBag } from "react-icons/fi";
+import {
+  CartButton,
+  ImageWrapper,
+  ProductContainer,
+} from "../Women/StyledWomenComponent";
 
-function menPage({ products }){
-  return(
+function menPage({ products }) {
+  return (
     <>
-    {
-      products.map(product=>{
-        if(product.category==="men's clothing"){
-          return(
-            
-            <Image src={product.image} width={300} height={400} alt="image" />
-          )
-        }
-      }
-      )
-    }
+      <HeaderContainer primary>
+        <Logo primary>ABC.</Logo>
+        <NavList>
+          <NavElements>MEN</NavElements>
+          <NavElements>WOMEN</NavElements>
+          <NavElements>SALE</NavElements>
+          <NavElements>NEW IN</NavElements>
+          <NavElements>DESIGNERS</NavElements>
+          <NavElements>THE JOURNAL</NavElements>
+          <NavElements>STYLES</NavElements>
+        </NavList>
+        <NavList>
+          <NavElements>MY ACCOUNT</NavElements>
+          <NavElements>
+            <FiSearch />
+          </NavElements>
+          <NavElements>
+            <FiShoppingBag />
+          </NavElements>
+        </NavList>
+      </HeaderContainer>
+      <ImageWrapper>
+        <ImageSection primary>
+          {products.map((product) => {
+            if (product.category === "men's clothing") {
+              return (
+                <ProductContainer>
+                  <ImageContainer primary key={product.id}>
+                    <Image
+                      src={product.image}
+                      width={200}
+                      height={300}
+                      alt="image"
+                    />
+                    <ImageNumber>{product.category}</ImageNumber>
+                    <ImageCaption primary>
+                      {`${
+                        product.title.length > 20
+                          ? product.title.substring(0, 30) + "..."
+                          : product.title
+                      }`}
+                      <ImageCaption>${product.price}</ImageCaption>
+                    </ImageCaption>
+                    <CartButton>ADD TO CART</CartButton>
+                  </ImageContainer>
+                </ProductContainer>
+              );
+            }
+          })}
+        </ImageSection>
+      </ImageWrapper>
     </>
-)}
+  );
+}
 
-export default menPage
+export default menPage;
 
-export async function getStaticProps(){
-  const response = await fetch('https://fakestoreapi.com/products');
-  const data = await response.json()
+export async function getStaticProps() {
+  const response = await fetch("https://fakestoreapi.com/products");
+  const data = await response.json();
 
-  return{
-    props:{
-      products:data
-    }
-  }
+  return {
+    props: {
+      products: data,
+    },
+  };
 }
