476b07d5dd02f9c4de611abaa5f23cf9164f9fd1
dragg54 <ajibolasadiq@yahoo.com>
2022-05-23 23:23:29 +0100

    added functionality to the cart element

--- component/Hero/index.js
+++ component/Hero/index.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FiSearch, FiShoppingBag } from "react-icons/fi";
 
 import {
+  CartLogo,
   HeaderContainer,
   HeroCaptionContainer,
   HeroContainer,
@@ -10,10 +11,19 @@ import {
   Logo,
   NavElements,
   NavList,
+  NoOfCartedProducts,
   PrimaryButton,
 } from "./StyledHero";
 
 function Header() {
+  const[localProducts, setLocalProducts] = useState([])
+
+  useEffect(()=>{
+    const localProducts = JSON.parse(localStorage.getItem("storedProducts"))
+      if(localProducts){
+        setLocalProducts(localProducts)
+      }
+  },[]) 
   return (
     <HeroContainer>
       <HeaderContainer>
@@ -28,18 +38,26 @@ function Header() {
           <NavElements>STYLES</NavElements>
         </NavList>
         <NavList>
-        <NavElements>MY ACCOUNT</NavElements>
-        <NavElements>
-          <FiSearch />
-        </NavElements>
-        <NavElements>
-          <FiShoppingBag />
-        </NavElements>
-      </NavList>
+          <NavElements>MY ACCOUNT</NavElements>
+          <NavElements>
+            <FiSearch />
+          </NavElements>
+          <NavElements>
+            <CartLogo>
+              <FiShoppingBag />
+              <NoOfCartedProducts primary>
+                {localProducts? localProducts.length: 0}
+              </NoOfCartedProducts>
+            </CartLogo>
+          </NavElements>
+        </NavList>
       </HeaderContainer>
       <HeroCaptionContainer>
         <HeroPrimaryCaption>TIS THE SEASON</HeroPrimaryCaption>
-        <HeroSecondaryCaption>SHOP OUR SELECTION OF UNIQUE GIFTS<br/> GUARANTEED TO MAKE YOU SMILE.</HeroSecondaryCaption>
+        <HeroSecondaryCaption>
+          SHOP OUR SELECTION OF UNIQUE GIFTS
+          <br /> GUARANTEED TO MAKE YOU SMILE.
+        </HeroSecondaryCaption>
         <PrimaryButton>SHOP NOW</PrimaryButton>
       </HeroCaptionContainer>
     </HeroContainer>
