610311b65eafceaa6c993404bce15272d43bc78c
dragg54 <ajibolasadiq@yahoo.com>
2022-05-25 00:24:39 +0100

    initial commit

---
+++ component/Section/NewProductSection/StyledNewProduct.js
@@ -0,0 +1,83 @@
+import styled from 'styled-components'
+
+export const NewProductContainer = styled.div`
+    width: 100%;
+    margin-top: 30px;
+    padding: 80px;
+
+`
+export const Header = styled.div`
+    font-size: 2.6rem;
+    font-weight: bolder;
+`
+
+export const NewProductSection = styled.div `
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    margin-top: 38px;
+`
+
+export const NewProductImageContainer = styled.div`
+    width: 600px;
+    height: 700px;
+    position: relative;
+`
+
+export const NewProducImgDecrContainer= styled.div`
+    width: 450px;
+    height: 900px;
+`
+
+export const DescrContainer = styled.div`
+    width: 100%;
+    padding: 20px 0 20px 0;
+    margin-top: 10px;
+`
+
+export const DescrHeader = styled.h4`
+    font-size: 1.4rem;
+    font-weight: bold;
+`
+
+export const Desc = styled.p`
+    font-size: 0.65rem;
+`
+
+export const NewInContainer = styled.div`
+    width: 200px;
+    height: 200px;
+    border: 2px  solid #ff6f1d ;
+    position: absolute;
+    border-radius: 50%;
+    top:35%;
+    bottom:50%;
+    right: -100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const NewInCaption = styled.p`
+    color: #ff6f1d;
+    font-size: 0.7rem;
+    font-family: bolder;
+`
+
+export const NewProductName = styled.h1`
+    color:  #ff6f1d;
+    font-size: 12rem;
+    font-weight: bolder;
+    position: absolute;
+    bottom: -250px;
+    left: -50px
+`
+
+export const BuyNowButton = styled.button`
+    width: 150px;
+    font-size: 0.7rem;
+    background-color: black;
+    padding: 14px;
+    color: white;
+    margin-top: 10px;
+`
---
+++ component/Section/NewProductSection/index.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Image from "next/image";
+import New from "../../../assets/images/New.jpg";
+import Coco from "../../../assets/images/coco.jpg";
+import {
+  BuyNowButton,
+  Desc,
+  DescrContainer,
+  DescrHeader,
+  Header,
+  NewInCaption,
+  NewInContainer,
+  NewProducImgDecrContainer,
+  NewProductContainer,
+  NewProductImageContainer,
+  NewProductName,
+  NewProductSection,
+} from "./StyledNewProduct";
+
+function NewProduct() {
+  return (
+    <NewProductContainer>
+      <Header>NEW PRODUCT</Header>
+      <NewProductSection>
+        <NewProductImageContainer>
+          <Image src={New} width={600} height={700} alt="img" />
+          <NewInContainer>
+            <NewInCaption>NEW IN</NewInCaption>
+          </NewInContainer>
+          <NewProductName>bleak</NewProductName>
+        </NewProductImageContainer>
+        <NewProducImgDecrContainer>
+          <Image src={Coco} width={450} height={400} />
+          <DescrContainer>
+            <DescrHeader>DESCRIPTION</DescrHeader>
+            <Desc>
+              Chanel Coco Mademoiselle is a Chypre Floral fragrance for women.
+              Coco Mademoiselle was launched in 2001. The nose behind this
+              fragrance is Jacques Polge. Top notes are orange, mandarin orange,
+              orange blossom and bergamot; middle notes are mimose, jasmine,
+              turkish rose and ylang-ylang; base notes are tonka bean,
+              patchouli, opoponax, vanilla, vetiver and white musk. This perfume
+              is the winner of award FiFi Award Best National Advertising
+              Campaign / TV 2008.
+            </Desc>
+            <BuyNowButton>BUY NOW</BuyNowButton>
+          </DescrContainer>
+        </NewProducImgDecrContainer>
+      </NewProductSection>
+    </NewProductContainer>
+  );
+}
+
+export default NewProduct;
