48b7018a2f92bce33091f7dbe33ed7f80a94aa31
dragg54 <ajibolasadiq@yahoo.com>
2022-05-25 00:22:52 +0100

    added new product section"

--- component/Section/index.js
+++ component/Section/index.js
@@ -8,34 +8,43 @@ import {
   ImageContainer,
   ImageNumber,
   ImageSection,
+  Liner,
+  LinerContainer,
 } from "./StyledSection";
 import Link from "next/link";
+import NewProduct from "./NewProductSection";
 
 const Section = () => {
   return (
-    <ImageSection>
-      <ImageContainer>
-        <Image src={Men} width={300} height={400} alt="image" />
-        <ImageNumber>1</ImageNumber>
-        <Link href=".../../Men" passHref>
-          <ImageCaption>MEN</ImageCaption>
-        </Link>
-      </ImageContainer>
-      <ImageContainer>
-        <Image src={Women} width={300} height={400} alt="image" />
-        <ImageNumber>2</ImageNumber>
-        <Link href=".../../Women" passHref>
-          <ImageCaption>WOMEN</ImageCaption>
-        </Link>
-      </ImageContainer>
-      <ImageContainer>
-        <Image src={Accessories} width={300} height={400} alt="image" />
-        <ImageNumber>3</ImageNumber>
-        <Link href="">
-          <ImageCaption>ACCESSORIES</ImageCaption>
-        </Link>
-      </ImageContainer>
-    </ImageSection>
+    <>
+      <ImageSection>
+        <ImageContainer>
+          <Image src={Men} width={300} height={400} alt="image" />
+          <ImageNumber>1</ImageNumber>
+          <Link href=".../../Men" passHref>
+            <ImageCaption>MEN</ImageCaption>
+          </Link>
+        </ImageContainer>
+        <ImageContainer>
+          <Image src={Women} width={300} height={400} alt="image" />
+          <ImageNumber>2</ImageNumber>
+          <Link href=".../../Women" passHref>
+            <ImageCaption>WOMEN</ImageCaption>
+          </Link>
+        </ImageContainer>
+        <ImageContainer>
+          <Image src={Accessories} width={300} height={400} alt="image" />
+          <ImageNumber>3</ImageNumber>
+          <Link href="">
+            <ImageCaption>ACCESSORIES</ImageCaption>
+          </Link>
+        </ImageContainer>
+      </ImageSection>
+      <LinerContainer>
+        <Liner></Liner>
+      </LinerContainer>
+      <NewProduct/>
+    </>
   );
 };
 
