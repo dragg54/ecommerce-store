dcfa6aad7a720a3c3ccd9ecd512fdf483049f12d
dragg54 <ajibolasadiq@yahoo.com>
2022-05-26 01:25:11 +0100

    changed image size

--- component/Section/index.js
+++ component/Section/index.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Men from "../../assets/images/men.jpg";
 import Women from "../../assets/images/woman.jpg";
 import Accessories from "../../assets/images/watch.jpg";
+import Gadgets from "../../assets/images/gadgets.jpg";
 import styled from "styled-components";
 import {
   ImageCaption,
@@ -19,26 +20,33 @@ const Section = () => {
     <>
       <ImageSection>
         <ImageContainer>
-          <Image src={Men} width={300} height={400} alt="image" />
+          <Image src={Men} width='400px' height='500px' alt="image" />
           <ImageNumber>1</ImageNumber>
           <Link href=".../../Men" passHref>
             <ImageCaption>MEN</ImageCaption>
           </Link>
         </ImageContainer>
         <ImageContainer>
-          <Image src={Women} width={300} height={400} alt="image" />
+          <Image src={Women} width={400} height={500} alt="image" />
           <ImageNumber>2</ImageNumber>
           <Link href=".../../Women" passHref>
             <ImageCaption>WOMEN</ImageCaption>
           </Link>
         </ImageContainer>
         <ImageContainer>
-          <Image src={Accessories} width={300} height={400} alt="image" />
+          <Image src={Accessories} width={400} height={500} alt="image" />
           <ImageNumber>3</ImageNumber>
           <Link href="">
             <ImageCaption>ACCESSORIES</ImageCaption>
           </Link>
         </ImageContainer>
+        <ImageContainer>
+        <Image src={Gadgets} width={400} height={500} alt="image" />
+        <ImageNumber>3</ImageNumber>
+        <Link href="">
+          <ImageCaption>GADGETS</ImageCaption>
+        </Link>
+      </ImageContainer>
       </ImageSection>
       <LinerContainer>
         <Liner></Liner>
