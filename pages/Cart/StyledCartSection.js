222c582d439b3f4eb51bfb250c9e78e652fc6d04
dragg54 <ajibolasadiq@yahoo.com>
2022-05-28 23:58:28 +0100

    changed color

--- pages/Cart/StyledCartSection.js
+++ pages/Cart/StyledCartSection.js
@@ -8,11 +8,160 @@ export const CartContainer = styled.div`
     align-items: center;
     justify-content: space-between;
     background-color: #EEEDE7;
+
+    @media(max-width:900px){
+        padding: 12px;
+    }
+    
 `
 
 export const CartProductsContainer = styled.div`
-    width: 80%;
+    width: 90%;
     height: 300px;
     margin-top: 9px;
     background-color: white;
+    display: flex;
+    padding:20px 15px 20px 15px;
+    align-items: center;
+    justify-content: space-between;
+
+    @media(max-width:900px){
+        height: 240px;
+    }
+
+    @media(max-width: 480px) {
+       margin-top: 5px;
+       padding: 12px 15px;
+       margin-bottom: 0;
+  }
+`
+
+export const CartHeader = styled.div`
+    font-size: 2.6rem;
+    width: 80%;
+    height: 80px;
+    margin-top: 9px;
+    background-color: white;
+    padding: 10px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+
+    @media(max-width:900px){
+        font-size: 2.0rem;
+        height: 60px;
+    }
+`
+
+export const CartImgContainer = styled.div`
+    width: 280px;
+    height: 300px;
+    object-fit: contain;
+    margin-right: 12px;
+    padding-top: 15px;
+    margin-top: 20px;
+
+    @media(max-width: 900px) {
+        padding-top: 50px;
+  }
+
+  @media(max-width: 600px) {
+        padding-top: 60px;
+  }
+`
+
+export const InStock = styled.p`
+    font-size: 0.6rem;
+    color: #8BCD50;
+`
+
+export const CartBtnContainer = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+`
+
+export const ProductContent = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+export const ProductDescr = styled.p`
+    font-size: 1rem;
+
+    @media(max-width:900px){
+        font-size: 0.8rem;
+    }
+
+    @media(max-width: 600px) {
+        font-size: 0.7rem;
+  }
+
+`
+export const EmptyCartContainer = styled.div`
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 40px;
+`
+
+export const EmptyCartCaption = styled.h2`
+    color: #821D30;
+    font-size: 4rem;
+    font-weight: bolder;
+`
+
+export const SelectBtn = styled.select`
+    font-size: 15px;
+    width: 100px;
+    outline: none;
+    padding: 4px 6px 4px 6px;
+    text-align: center;
+    border-radius: 10px;
+    margin-right: 5px;
+
+    @media(max-width: 768px) {
+        font-size: 0.7rem;
+        width: 85px;
+        padding:4px 
+  }
+  @media(max-width: 480px) {
+        width: 60px;
+        padding:3px 4px
+  }
+`
+export const Price = styled.p`
+    font-size: 1.5rem;
+    font-weight: bold;
+    height: 15px;
+    margin-bottom: 0;
+
+    @media(max-width: 480px) {
+        margin-top: 6px;
+        font-size: 1.2rem;
+  }
+    
+`
+
+
+
+export const DeleteBtn = styled.button`
+    font-size: 15px;
+    padding: 5px 8px 5px 8px;
+    border-radius: 10px;
+    margin: 0 5px 0 5px;
+    outline: none;
+    border: none;
+    width: 60px;
+
+    @media(max-width: 768px) {
+        font-size: 12px;
+  }
+`
+
+export const Slash = styled.span`
+    opacity: 0.3;
+    color: #444444;
 `
--- pages/Cart/index.js
+++ pages/Cart/index.js
@@ -1,22 +1,165 @@
-import React, {useState, useEffect} from 'react'
-import { CartContainer, CartProductsContainer } from './StyledCartSection'
+import React, { useState, useEffect } from "react";
+import {
+  ImageCaption,
+  ImageNumber,
+  ImageSection,
+} from "../../component/Section/StyledSection";
+import {
+  HeaderContainer,
+  Logo,
+  NavList,
+  NavElements,
+  CartLogo,
+  NoOfCartedProducts,
+  MenuIcon,
+  NavIcons,
+  CartIcon,
+} from "../../component/Hero/StyledHero";
+import { FiMenu, FiSearch, FiShoppingBag } from "react-icons/fi";
+import {
+  CartButton,
+  ImageWrapper,
+  ProductContainer,
+} from "../Women/StyledWomenComponent";
+import { BsCart3 } from "react-icons/bs";
+
+import {
+  BtnContainer,
+  CartBtnContainer,
+  CartContainer,
+  CartHeader,
+  CartImgContainer,
+  CartProductsContainer,
+  DeleteBtn,
+  EmptyCartCaption,
+  EmptyCartContainer,
+  EmptyCartImgContainer,
+  ImageContainer,
+  InStock,
+  Price,
+  ProductContent,
+  ProductDescr,
+  SelectBtn,
+  Slash,
+} from "./StyledCartSection";
+import EmptyCart from "../../assets/images/emptycart.jpg";
+import SadEmoji from "../../assets/images/emoji.jpg";
+import Image from "next/image";
 
 function Cart() {
-    const[cartProducts, setCartProducts] = useState([])
-    useEffect(()=>{
-        const cartProducts = JSON.parse(localStorage.getItem("storedProducts"))
-        if(cartProducts){
-            setCartProducts(cartProducts)
-        }
-    },[])
+  const [cartProducts, setCartProducts] = useState([]);
+  useEffect(() => {
+    const cartProducts = JSON.parse(localStorage.getItem("storedProducts"));
+    if (cartProducts) {
+      setCartProducts(cartProducts);
+    }
+   
+  }, [])
 
-    console.log(cartProducts)
-  return (
-    <CartContainer>
-        <CartProductsContainer>hello</CartProductsContainer>
-        <CartProductsContainer>hello</CartProductsContainer>
-    </CartContainer>
-  )
+  const deleteItem = id =>{
+    const cartItems = cartProducts.filter(cartProduct=>cartProduct.id != id)
+    if(cartItems){
+      localStorage.setItem("storedProducts", JSON.stringify(cartItems))
+      setCartProducts(cartItems)
+    }
+  }
+
+  if (cartProducts.length>0) {
+    return (
+      <>
+      <HeaderContainer primary>
+        <MenuIcon>
+          <FiMenu />
+        </MenuIcon>
+        <Logo primary>ABC.</Logo>
+        <NavIcons>
+          {" "}
+          <NavElements>MY ACCOUNT</NavElements>
+          <NavElements>
+            <FiSearch />
+          </NavElements>
+          <CartIcon>
+            <CartLogo>
+              <BsCart3 />
+              <NoOfCartedProducts primary>
+                {cartProducts ? cartProducts.length : 0}
+              </NoOfCartedProducts>
+            </CartLogo>
+          </CartIcon>
+        </NavIcons>
+        <NavList>
+          <NavElements>MEN</NavElements>
+          <NavElements>WOMEN</NavElements>
+          <NavElements>SALE</NavElements>
+          <NavElements>NEW IN</NavElements>
+          <NavElements>DESIGNERS</NavElements>
+          <NavElements>THE JOURNAL</NavElements>
+          <NavElements>STYLES</NavElements>
+        </NavList>
+        <NavList>
+          <NavElements>MY ACCOUNT</NavElements>
+          <NavElements>
+            <FiSearch />
+          </NavElements>
+          <NavElements>
+            <CartLogo>
+              <FiShoppingBag />
+              <NoOfCartedProducts>
+                {cartProducts ? cartProducts.length : 0}
+              </NoOfCartedProducts>
+            </CartLogo>
+          </NavElements>
+        </NavList>
+      </HeaderContainer>
+        <CartHeader>Shopping Cart</CartHeader>
+        {cartProducts.map((cartProduct) => {
+          return (
+            <div>
+              <CartContainer key={cartProduct.id}>
+                <CartProductsContainer>
+                  <CartImgContainer>
+                    <Image src={cartProduct.image} width={220} height={250} />
+                  </CartImgContainer>
+                  <ProductContent>
+                    <ProductDescr>
+                      {cartProduct.description.length > 150
+                        ? cartProduct.description.substring(0, 150) + "..."
+                        : cartProduct.description}
+                        <Price>${cartProduct.price}</Price>
+                    </ProductDescr>
+                    <div>
+                      <InStock>In Stock</InStock>
+                    </div>
+                    <CartBtnContainer>
+                        <SelectBtn class="select" name="option">
+                          <option value='0'>
+                            Qty:0
+                          </option>
+                          <option value="1">1</option>
+                          <option value="2">2</option>
+                          <option value="3">3</option>
+                        </SelectBtn>
+                        <Slash>|</Slash>
+                        <DeleteBtn onClick={()=>deleteItem(cartProduct.id)}>Delete</DeleteBtn>
+                    </CartBtnContainer>
+                  </ProductContent>
+                </CartProductsContainer>
+              </CartContainer>
+            </div>
+          );
+        })}
+      </>
+    );
+  } if(cartProducts.length==0){
+    return (
+      <EmptyCartContainer>
+        <Image src={EmptyCart} width={750} height={400} alt="empty cart" />
+        <EmptyCartCaption>
+          Cart Empty <Image src={SadEmoji} height={50} width={50} />
+        </EmptyCartCaption>
+      </EmptyCartContainer>
+    );
+  }
 }
 
-export default Cart
+export default Cart;
