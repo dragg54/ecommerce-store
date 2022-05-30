2dca5ac36b00e6455975b2fef67cc1d456559466
dragg54 <ajibolasadiq@yahoo.com>
2022-05-30 18:43:37 +0100

    added new section

--- component/Section/index.js
+++ component/Section/index.js
@@ -14,6 +14,7 @@ import {
 } from "./StyledSection";
 import Link from "next/link";
 import NewProduct from "./NewProductSection";
+import BestPrice from "./BestPrice";
 
 const Section = () => {
   return (
@@ -52,6 +53,7 @@ const Section = () => {
         <Liner></Liner>
       </LinerContainer>
       <NewProduct/>
+      <BestPrice />
     </>
   );
 };
